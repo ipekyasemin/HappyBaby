5d39606c507e055dd4dd3be918cc43dbc1835f1a
ipekyasemin <ipekyasemin958@gmail.com>
2025-03-05 23:06:07 +0300

    Initial Commit

---
+++ HappyBaby/HappyBabyApp.swift
@@ -0,0 +1,17 @@
+//
+//  HappyBabyApp.swift
+//  HappyBaby
+//
+//  Created by yasemin ipek on 5.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct HappyBabyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
